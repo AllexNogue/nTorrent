844fb979f414b0b42f7041083d6d797959b76e8f
Allex Nogue <alexhermom@gmail.com>
2020-05-25 22:42:10 -0300

    A

--- send.js
+++ send.js
@@ -9,3 +9,13 @@ const urlParse = require('url').parse;
 const torrent = bencode.decode(fs.readFileSync('torrent.torrent'));
 
 const url = urlParse(torrent.announce.toString('utf8'));
+
+const socket = dgram.createSocket('udp4');
+
+const myMsg = Buffer.from('Oi maçã?', 'utf8');
+
+socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
+
+socket.on('message', msg => {
+    console.log('message is', msg);
+});
